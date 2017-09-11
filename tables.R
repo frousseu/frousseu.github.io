7e9d318f3a3e6dc43ef72470b94bf342cb588407
frousseu <francois.rousseu@usherbrooke.ca>
2017-09-11 09:35:52 -0400

    NA time to ""

--- tables.R
+++ tables.R
@@ -24,7 +24,7 @@ int2time<-function(x){
   h<-x%/%3600
   m<-(x-h*3600)/60
   res<-paste(formatC(h,width=2,flag=0),formatC(m,width=2,flag=0),sep=":")
-  ifelse(is.na(x),NA,res)
+  ifelse(is.na(x),"",res)
 }
 
 x<-as.data.frame(gsheet2tbl('https://docs.google.com/spreadsheets/d/1Udz3YHed2MMq7X5eeO8IByIJuyePUa51VKCctZr47IM/edit#gid=0'))
