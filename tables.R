4b630011621d60692201979a0658ce4d6db7fdca
frousseu <francois.rousseu@usherbrooke.ca>
2018-04-16 09:56:36 -0400

    better cat

--- tables.R
+++ tables.R
@@ -60,6 +60,11 @@ x$Date<-format(as.Date(x$Date),"%d %b")
 
 align<-paste(paste(rep('c',ncol(x)-2),collapse=''),paste(rep("l",2),collapse=""),sep="")  
 
-htmlTable(x,rnames=FALSE,css.cell=css.cell,align=align)
-
-
+bab<-function(){
+  cat("\014")
+  cat("<!DOCTYPE html>\n<html>\n<head>\n<link href='styles.css' rel='stylesheet' type='text/css'>\n</head>\n<body>\n")
+  h<-htmlTable(x,rnames=FALSE,css.cell=css.cell,align=align)
+  print(h)
+  cat("</body>\n</html>")
+}
+bab()
