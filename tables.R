6d38fe95994a7fc32c6e87e21f1240f73f663155
frousseu <francois.rousseu@usherbrooke.ca>
2019-11-28 11:56:07 -0500

    links for person and activity and activities removed with end time

--- tables.R
+++ tables.R
@@ -12,12 +12,15 @@ library(htmlTable)
 
 
 clink<-function(x,col){
-  ifelse(!is.na(x[,"Lien"]),paste0("<a target='_blank' href='",x[,"Lien"],"'>",x[,col],"</a>"),x[,col])
+  if(col=="Présentateur / Responsable"){
+    return(ifelse(!is.na(x[,"Lien_Pres"]),paste0("<a target='_blank' href='",x[,"Lien_Pres"],"'>",x[,col],"</a>"),x[,col]))
+  }
+  if(col=="Détails"){
+    return(ifelse(!is.na(x[,"Lien_Details"]),paste0("<a target='_blank' href='",x[,"Lien_Details"],"'>",x[,col],"</a>"),x[,col]))
+  }
 }
 
 int2time<-function(x){
-  #x<-as.integer(x)
-  #x<-ifelse(is.na(x),"",x)
   x<-sapply(strsplit(format(x),":"),function(i){
     if(length(i)==1){
       NA
@@ -34,26 +37,26 @@ int2time<-function(x){
 }
 
 x<-as.data.frame(gsheet2tbl('https://docs.google.com/spreadsheets/d/1Udz3YHed2MMq7X5eeO8IByIJuyePUa51VKCctZr47IM/edit#gid=0'))
-x<-x[!is.na(x$Événement) & as.character(x$Date)>=substr(Sys.time()-(3600*24*0),1,10),]
+end_time<-as.POSIXct(ifelse(is.na(x$Fin),NA,paste(x$Date,x$Fin)),format="%Y-%m-%d %H:%M:%OS")
+keep<-ifelse(is.na(end_time),as.character(x$Date)>=substr(Sys.time(),1,10),end_time>=Sys.time())
+x<-x[!is.na(x$Événement) & keep,]
 names(x)<-gsub("\\..."," / ",names(x))
-
 
 day<-weekdays(as.Date(x$Date))
 x$Jour<-paste0(toupper(substr(day,1,1)),substr(day,2,nchar(day)))
 
-x$Détails<-ifelse(x$Événement=="Séminaire invité",x$Détails,clink(x,col="Détails"))
-x$"Présentateur / Responsable"<-ifelse(x$Événement=="Séminaire invité",clink(x,col="Présentateur / Responsable"),x$"Présentateur / Responsable")
+x$Détails<-clink(x,col="Détails")
+x$"Présentateur / Responsable"<-clink(x,col="Présentateur / Responsable")
 x$Début<-int2time(x$Début)
 x$Fin<-int2time(x$Fin)
 x$Lien<-NA
 x<-x[order(x$Date,x$Début),]
-x<-x[,setdiff(names(x),c("Lien","Commentaires"))]
+x<-x[,setdiff(names(x),c("Lien_Pres","Lien_Details","Commentaires"))]
 x<-x[,c("Date","Jour","Début","Fin","Événement","Présentateur / Responsable","Local","Détails")]
 
 css.cell1<-matrix("padding-left: .9em; padding-right: .9em;",ncol=ncol(x),nrow=nrow(x))
 css.cell2<-matrix("",ncol=ncol(x),nrow=nrow(x))
 cw <- strftime(as.POSIXlt(as.character(x$Date)),format="%W")%in%strftime(as.POSIXlt(substr(Sys.time(),1,10)),format="%W") 
-#css.cell2[cw,]<-" font-weight: bolder;"
 css.cell2[cw,]<-""
 css.cell<-matrix(paste0(css.cell1,css.cell2),ncol=ncol(x),nrow=nrow(x))
 
